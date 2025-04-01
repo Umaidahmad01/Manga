d73b67e134fc45f1a419d9a1a9351a7be4033351
Umaid <ahmedumaid707@gmail.com>
2025-04-02 01:19:09 +0530

    Update and rename Database to confi.py

---
+++ confi.py
@@ -0,0 +1,7 @@
+# config.py
+MONGO_URI = "mongodb://localhost:27017/"  # Apna MongoDB URI daal
+DB_NAME = "manga_scraper"
+LOG_CHANNEL_ID = "-1001234567890"  # Telegram channel/group ID
+OWNER_ID = "987654321"  # Owner ka Telegram user ID
+API_ID = "1234567"  # Telegram API ID (my.telegram.org se)
+API_HASH = "abcdefghijklmnopqrstuvwxyz123456"  # Telegram API Hash
