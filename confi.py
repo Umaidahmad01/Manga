8a38aaef9417d2be2fe838644d0a50b72bb9866f
Umaid <ahmedumaid707@gmail.com>
2025-04-02 01:34:58 +0530

    Update confi.py

--- confi.py
+++ confi.py
@@ -1,7 +1,6 @@
 # config.py
-MONGO_URI = "mongodb://localhost:27017/"  # Apna MongoDB URI daal
-DB_NAME = "manga_scraper"
-LOG_CHANNEL_ID = "-1001234567890"  # Telegram channel/group ID
-OWNER_ID = "987654321"  # Owner ka Telegram user ID
-API_ID = "1234567"  # Telegram API ID (my.telegram.org se)
-API_HASH = "abcdefghijklmnopqrstuvwxyz123456"  # Telegram API Hash
+MONGO_URI = "mongodb+srv://spxsolo:umaid2008@cluster0.7fbux.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # MongoDB URI
+DB_NAME = "cluster0"
+LOG_CHANNEL_ID = "-1002078429106"  # Telegram channel/group ID
+OWNER_ID = "5585016974"  # Owner Telegram ID
+BOT_TOKEN = "7403693425:AAHaGlkp-zNNPvNeO62xWqwmsRI5apY0Dcs"  # Apna Telegram Bot Token
